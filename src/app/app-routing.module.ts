fd30d4b58988c3e89b54e3414b4617d11180a876
abbalon92 <sergiomhlopez@gmail.com>
2018-10-07 22:12:45 -0500

    ajuste del menu para los modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,15 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },  { path: 'auditory', loadChildren: './auditory/auditory.module#AuditoryPageModule' },
+  { path: 'check-seller', loadChildren: './check-seller/check-seller.module#CheckSellerPageModule' },
+  { path: 'auditory-stand-info', loadChildren: './auditory-stand-info/auditory-stand-info.module#AuditoryStandInfoPageModule' },
+  { path: 'auditory-questionary', loadChildren: './auditory-questionary/auditory-questionary.module#AuditoryQuestionaryPageModule' },
+  { path: 'auditory-evidence', loadChildren: './auditory-evidence/auditory-evidence.module#AuditoryEvidencePageModule' },
+  { path: 'novelty-create', loadChildren: './novelty-create/novelty-create.module#NoveltyCreatePageModule' },
+  { path: 'novelty-detail', loadChildren: './novelty-detail/novelty-detail.module#NoveltyDetailPageModule' },
+  { path: 'novelty-evidence', loadChildren: './novelty-evidence/novelty-evidence.module#NoveltyEvidencePageModule' }
+
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+	modulos:Array<{name:string,pagina:string}>;
+
+	constructor(private router:Router){
+		this.modulos=[
+		{name:"mi puesto",pagina:"/home"},
+		{name:"Cetificado",pagina:"/home"},
+		{name:"pqr",pagina:"/home"},
+		{name:"capacitacion",pagina:"/home"},
+		{name:"auditorias",pagina:"/auditory"}
+		];
+	}
+
 
 }
