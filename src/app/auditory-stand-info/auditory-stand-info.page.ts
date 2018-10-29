a0248fd439c8abeb37ee2ced246329215d293643
abbalon92 <sergiomhlopez@gmail.com>
2018-10-28 20:13:33 -0500

    ajuste al modulo de auditoria sobre el uso del qr y traer la informacion del puesto

--- src/app/auditory-stand-info/auditory-stand-info.page.ts
+++ src/app/auditory-stand-info/auditory-stand-info.page.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LoadingController } from '@ionic/angular';
+import { StandService } from "../service/stand.service";
+
 @Component({
   selector: 'app-auditory-stand-info',
   templateUrl: './auditory-stand-info.page.html',
   styleUrls: ['./auditory-stand-info.page.scss'],
 })
 export class AuditoryStandInfoPage implements OnInit {
+  
   title:string="Informacion del puesto";
-  constructor() { }
+  stand:any[]=[];
+  
+  constructor(private loading_Controller:LoadingController,
+    private standService:StandService
+    ) { }
 
   ngOnInit() {
+    this.getUser();
   }
+
+
+  public async getUser() {
+    const loading = await this.loading_Controller.create({
+      message: 'Loading'
+    });
+    await loading.present();
+    this.standService.getPuesto('1')
+      .subscribe(res => {
+        this.stand=res;
+        loading.dismiss();
+      }, err => {
+        console.log(err);
+        loading.dismiss();
+      });
+  }
+
 
 }
