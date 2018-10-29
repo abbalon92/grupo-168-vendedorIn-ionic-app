11d91d1eef2a373da127dc1464ad31e772a860d7
Andres Castillo <elidibs90@gmail.com>
2018-10-29 00:33:52 -0500

    Se incorporan los servicios reemplazando la mayor parte de Texto quemado existente

--- src/app/clothes-management/clothes-management.page.ts
+++ src/app/clothes-management/clothes-management.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router,ActivatedRoute } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
+import { CourseService } from '../service/course.service';
+import { TrainingService } from '../service/training.service';
 @Component({
   selector: 'app-clothes-management',
   templateUrl: './clothes-management.page.html',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClothesManagementPage implements OnInit {
 
-	titlePage:string = "Gestión de textiles o ropa";
-  constructor() {  }
-  ngOnInit() {
-  }
+  titlePage:string = "Gestión de atención al cliente";
+  courses:any;
 
+  moduleCourseCM=5;
+  constructor(private router: Router,public route: ActivatedRoute,public courseService: CourseService, public trainingsService: TrainingService ,public loadingController: LoadingController) { }
+
+  ngOnInit() {
+    this.getCourse();
+  }
+  getCourse() {
+    this.courseService.getCourse(this.moduleCourseCM)
+    .then(data => {
+      this.courses = data;
+     // console.log(this.trainings);
+    });
+  }
 }
--- src/app/food-management/food-management.page.ts
+++ src/app/food-management/food-management.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router,ActivatedRoute } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
+import { CourseService } from '../service/course.service';
+import { TrainingService } from '../service/training.service';
 @Component({
   selector: 'app-food-management',
   templateUrl: './food-management.page.html',
   styleUrls: ['./food-management.page.scss'],
 })
 export class FoodManagementPage implements OnInit {
-	titlePage:string = "Manipulación de Alimentos";
-  constructor() { }
+  titlePage:string = "Manipulación de Alimentos";
+  courses:any;
+
+  moduleCourseFM=1;
+
+  constructor(private router: Router,public route: ActivatedRoute,public courseService: CourseService, public trainingsService: TrainingService ,public loadingController: LoadingController) { }
 
   ngOnInit() {
+    this.getCourse();
   }
-
+  getCourse() {
+    this.courseService.getCourse(this.moduleCourseFM)
+    .then(data => {
+      this.courses = data;
+     // console.log(this.trainings);
+    });
+  }
 }
